f5c2e44918cf2429e61d871e80a4a3ce518584b5
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2025-05-05 23:14:23 +0700

    fix: justify for desktop display on detail page

--- src/presentation/features/Detail/DetailPage.tsx
+++ src/presentation/features/Detail/DetailPage.tsx
@@ -49,41 +49,48 @@ export default function DetailPage() {
   return (
     <div className="p-4">
       <HeaderBack title="Detail" />
-      <div className="aspect-square w-full mb-4 relative">
-        <Image
-          src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
-          alt={artwork.title}
-          fill
-          className="object-cover rounded"
-        />
-      </div>
 
-      <div className="flex justify-between items-center mb-2">
-        <p className="text-sm text-gray-500">Credit: {artwork.credit_line}</p>
-        <Button onClick={toggleSave} disabled={saved}>
-          {saved ? "Saved!" : "Save"}
-        </Button>
-      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-4">
+        <div className="w-full aspect-square relative">
+          <Image
+            src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
+            alt={artwork.title}
+            fill
+            className="object-cover rounded"
+          />
+        </div>
 
-      <div className="space-y-4">
-        <DetailSection title="Description">
-          {artwork.thumbnail?.alt_text || "N/A"}
-        </DetailSection>
-        <DetailSection title="Provenance Text">
-          <ExpandableText text={artwork.provenance_text || "N/A"} />
-        </DetailSection>
-        <DetailSection title="Publication History">
-          <ExpandableText text={artwork.publication_history || "N/A"} />
-        </DetailSection>
-        <DetailSection title="Exhibition History">
-          <ExpandableText text={artwork.exhibition_history || "N/A"} />
-        </DetailSection>
-        <DetailSection title="Artist">
-          {artwork.artist_display || "N/A"}
-        </DetailSection>
-        <DetailSection title="Date">
-          {artwork.date_display || "N/A"}
-        </DetailSection>
+        <div>
+          <div className="flex justify-between items-center mb-2">
+            <p className="text-sm text-gray-500">
+              Credit: {artwork.credit_line}
+            </p>
+            <Button onClick={toggleSave} disabled={saved}>
+              {saved ? "Saved!" : "Save"}
+            </Button>
+          </div>
+
+          <div className="space-y-4">
+            <DetailSection title="Description">
+              {artwork.thumbnail?.alt_text || "N/A"}
+            </DetailSection>
+            <DetailSection title="Provenance Text">
+              <ExpandableText text={artwork.provenance_text || "N/A"} />
+            </DetailSection>
+            <DetailSection title="Publication History">
+              <ExpandableText text={artwork.publication_history || "N/A"} />
+            </DetailSection>
+            <DetailSection title="Exhibition History">
+              <ExpandableText text={artwork.exhibition_history || "N/A"} />
+            </DetailSection>
+            <DetailSection title="Artist">
+              {artwork.artist_display || "N/A"}
+            </DetailSection>
+            <DetailSection title="Date">
+              {artwork.date_display || "N/A"}
+            </DetailSection>
+          </div>
+        </div>
       </div>
     </div>
   );
